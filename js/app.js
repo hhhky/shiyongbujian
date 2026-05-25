fa1ad29c134d9d43764c40f53258756f866685ab
hhhky <hhh1416837120@gmail.com>
2026-05-25 14:15:06 +0800

    修复侧边栏子导航退出后仍然显示的问题：改用内联 style.display 控制显隐

--- js/app.js
+++ js/app.js
@@ -60,7 +60,7 @@ function enterWidget(id) {
 
   // Sidebar: show back link + widget nav
   document.getElementById('sidebar-back-btn').classList.remove('hidden');
-  document.querySelectorAll('#sidebar-nav .sidebar-sub').forEach(function(s){ s.classList.remove('hidden'); });
+  document.querySelectorAll('.sidebar-sub').forEach(function(s){ s.style.display = ''; });
 
   // Reset to categories tab
   switchTab('categories');
@@ -86,7 +86,7 @@ function goHome() {
 
   // Sidebar
   document.getElementById('sidebar-back-btn').classList.add('hidden');
-  document.querySelectorAll('#sidebar-nav .sidebar-sub').forEach(function(s){ s.classList.add('hidden'); });
+  document.querySelectorAll('.sidebar-sub').forEach(function(s){ s.style.display = 'none'; });
 
   closePreview();
   renderWidgets();
